218b6da73e421d9dd88eedec5d269e97c28f0847
putriewfauziahr <putrifauziahrahman@gmail.com>
2024-09-30 00:15:20 +0700

    Initial Commit

---
+++ cypress/support/E2E/login.cy.js
@@ -0,0 +1,11 @@
+require('@cypress/xpath');
+import "../commands";
+import loginPage from '../../support/pageObjects/loginPageObject';
+
+describe('Login', () => {
+    it('Success Login', () => {
+    
+    });
+
+
+});
